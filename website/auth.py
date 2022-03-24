f1c7caf6df109706f4ce1c6c89f474dd122ff200
snbianco <samantha.n.bianco@vanderbilt.edu>
2022-03-24 11:05:47 -0500

    Some stuff

--- website/auth.py
+++ website/auth.py
@@ -20,9 +20,8 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                return redirect(url_for('auth.nav_to_menu'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -141,6 +140,6 @@ def sign_up():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('auth.nav_to_menu'))
 
     return render_template("sign_up.html", user=current_user)
