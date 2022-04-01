d18280224b52363b997078866a0920b9098dba6e
snbianco <samantha.n.bianco@vanderbilt.edu>
2022-03-31 20:12:51 -0500

    More Changes

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, send_file
-from pyrsistent import v
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User, Music
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
